8bbad581b6b06e5243fe36be22c21ac79c3d8cba
ahmadmuhbit <ahmadmuhbit@gmail.com>
2021-10-06 19:04:46 +0700

    Navigation - Create Home Controller, Route Web, Edit View Welcome, Products & Edit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@home');
+Route::get('/products', 'HomeController@viewProduct');
+Route::get('/edit', 'HomeController@edit');
