c36380718ac36176353f952339cc32a1d0d12148
ahmadmuhbit <ahmadmuhbit@gmail.com>
2021-10-06 19:59:16 +0700

    Create - 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@home');
-Route::get('/products', 'HomeController@viewProduct');
-Route::get('/edit', 'HomeController@edit');
+Route::get('/', 'ProductController@home');
+Route::get('/products', 'ProductController@viewProduct');
+Route::get('/edit', 'ProductController@edit');
+
+Route::post('/store', 'ProductController@store');
